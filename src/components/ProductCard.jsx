09f9f6aa682365e0641b7aa392657c039871212a
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-12-07 00:12:09 +0600

    Test

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -13,7 +13,10 @@ const ProductCard = ({ product }) => {
       <p>{product.name}</p>
       <div className="flex justify-between items-center">
         <p className="text-red-900 font-bold text-2xl">৳{product.price}</p>
-        <button className="bg-red-900 rounded-lg p-1 text-white cursor-pointer">
+        <button
+          className="bg-red-900 rounded-lg p-1 text-white cursor-pointer"
+          onClick={() => console.log("Add to cart is clicked")}
+        >
           Add to Cart
         </button>
       </div>
