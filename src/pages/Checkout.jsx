9d8e2f491953954305774870d5088d88c797c405
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-12-09 02:09:31 +0600

    Update: Redesinged The Checkout Page for better look

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,11 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import CartItem from "../components/CartItem";
+import { MdDeleteForever } from "react-icons/md";
 import { GiTakeMyMoney } from "react-icons/gi";
 
 export default function Checkout() {
+  const { removeFromCart } = useContext(CartContext);
   const { cart } = useContext(CartContext);
   const subTotal = cart.reduce(
     (sum, item) => sum + item.quantity * item.price,
@@ -12,110 +14,114 @@ export default function Checkout() {
   const deliveryCharge = 60;
   const total = subTotal + deliveryCharge;
   return (
-    <div className="bg-primary/10 h-full w-screen md:w-[80%]  md:mx-auto p-2 md:p-5 md:pt-10 flex flex-col md:flex md:flex-row gap-5 md:gap-10 justify-between">
-      {/* Product Summary */}
-      <div className="w-full rounded-lg p-2 bg-white md:w-[50%] shadow-xl/10">
-        <h2 className="text-2xl font-medium pb-2">Product Summary</h2>
-        <div className="flex flex-col gap-3">
-          {cart.length === 0 && <p>No items yet...</p>}
-
-          {cart.map((item) => (
-            <CartItem key={item.code} item={item} />
-          ))}
-        </div>
-        <div className="flex justify-between items-center text-2xl font-medium h-15 border-b border-b-gray-300">
-          <h2>Subtotal:</h2>
-          <span>{subTotal}</span>
-        </div>
-        <div className="flex justify-between items-center text-2xl font-medium h-15 border-b border-b-gray-300">
-          <h2>Delivery Charge:</h2>
-          <span>{deliveryCharge}</span>
-        </div>
-        <div className="flex justify-between items-center text-2xl font-medium h-15">
-          <h2>Grand Total:</h2>
-          <span>{total}</span>
+    // main container
+    <div className="bg-white w-full flex flex-col justify-center lg:flex-row lg:gap-5 my-5 ">
+      {/* product summary */}
+      <div className="bg-gray-200 m-2 p-2 rounded-lg">
+        <h2 className="my-3 text-lg font-medium">Product Summary</h2>
+        {/* product cards + summaries*/}
+        <div className="flex flex-col gap-10">
+          {/* products cards */}
+          <div className="flex flex-col gap-2">
+            {cart.map((item) => (
+              <div
+                key={item.code}
+                className="bg-white rounded-lg p-2 flex gap-2 justify-between items-center"
+              >
+                {/* image */}
+                <div className="w-15 h-15 shrink-0 rounded overflow-hidden shadow">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                {/* item info + dlt button*/}
+                <div className="flex grow justify-between items-center gap-4">
+                  {/* item info */}
+                  <div className="flex flex-col justify-between">
+                    <h2 className="text-sm">{item.name}</h2>
+                    <p className="text-sm">Qty: {item.quantity}</p>
+                  </div>
+                  {/* delete button */}
+                  <button
+                    onClick={() => removeFromCart(item.code)}
+                    className="bg-gray-300 hover:bg-primary p-1 rounded-2xl shrink-0 cursor-pointer"
+                  >
+                    <MdDeleteForever size={20} />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Summaries */}
+          <div className="flex flex-col gap-2">
+            <div className="bg-white rounded-lg text-lg font-medium p-2 flex justify-between">
+              <p>Subtotal: </p>
+              {subTotal}
+            </div>
+            <div className="bg-white rounded-lg text-lg font-medium p-2 flex justify-between">
+              <p>Delivery Charge: </p>
+              {deliveryCharge}
+            </div>
+            <div className="bg-white rounded-lg text-lg font-medium p-2 flex justify-between">
+              <p>Grand Total: </p>
+              {total}
+            </div>
+          </div>
         </div>
       </div>
-      {/* Shipping Info */}
-      <div className="w-full flex flex-col gap-3 rounded-lg p-2 bg-white md:w-[50%] shadow-xl/10">
-        <div>
-          <form action="submit">
-            <div>
-              <h2 className="text-2xl font-medium pb-2">Contact Information</h2>
-
-              <label htmlFor="name">
-                Full Name<span className="text-primary text-2xl">*</span>
-              </label>
-              <br />
-              <input
-                type="text"
-                id="name"
-                required
-                className="bg-white ring-1 rounded w-full h-10 px-2 my-2"
-              />
-
-              <label htmlFor="name">Email Address </label>
-              <br />
-              <input
-                type="email"
-                id="email"
-                className="bg-white ring-1 rounded w-full h-10 px-2 my-2"
-              />
-
-              <label htmlFor="phone">
-                Phone Number <span className="text-primary text-2xl">*</span>
-              </label>
-              <br />
-              <input
-                type="number"
-                id="phone"
-                required
-                className="bg-white ring-1 rounded w-full h-10 px-2 my-2"
-              />
-              <label htmlFor="city">
-                City <span className="text-primary text-2xl">*</span>
-              </label>
-              <br />
-              <input
-                type="text"
-                id="city"
-                required
-                className="bg-white ring-1 rounded w-full h-10 px-2 my-2"
-              />
-              <label htmlFor="address">
-                Address <span className="text-primary text-2xl">*</span>
-              </label>
-              <br />
-              <input
-                type="text"
-                id="address"
-                required
-                className="bg-white ring-1 rounded w-full h-10 px-2 my-2"
-              />
-            </div>
+      {/* Shipping */}
+      <div className="w-full lg:w-100 flex flex-col">
+        {/* Shipping Info */}
+        <div className="bg-gray-200 m-2 p-2 rounded-lg">
+          <h2 className="my-3 text-lg font-medium">Shippping Details</h2>
+          <form action="submit" className="flex flex-col gap-3">
+            <input
+              type="text"
+              placeholder="Your Name *"
+              required
+              className="w-full h-8 px-2 ring-1 rounded bg-white"
+            />
+            <input
+              type="number"
+              placeholder="Phone *"
+              required
+              className="w-full h-8 px-2 ring-1 rounded bg-white"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full h-8 px-2 ring-1 rounded bg-white"
+            />
+            <input
+              type="text"
+              placeholder="Your City *"
+              required
+              className="w-full h-8 px-2 ring-1 rounded bg-white"
+            />
+            <input
+              type="text"
+              placeholder="Your Address *"
+              required
+              className="w-full h-8 px-2 ring-1 rounded bg-white"
+            />
           </form>
         </div>
-        <div className="w-full rounded-lg p-2 bg-white shadow-xl/10">
-          <h2 className="text-2xl font-medium pb-2 text-center">
-            Payment Details
-          </h2>
-          <div className="flex justify-start items-center gap-3">
-            <GiTakeMyMoney size={50} className="text-gray-600" />
-            <div className="flex flex-col">
-              <p className="text-lg font-medium">Cash on Delivery</p>
-              <p className="text-sm font-light">
-                পণ্য হাতে পেয়ে টাকা পরিশোধ করুন
-              </p>
+        {/* Payment Info */}
+        <div className="bg-gray-200 m-2 p-2 rounded-lg">
+          <h2 className="my-3 text-lg font-medium">Payment Details</h2>
+          <div className="bg-white rounded-lg text-lg font-medium p-2 flex justify-start items-center gap-5">
+            <GiTakeMyMoney size={30} />
+            <div>
+              <h2>Cash On Delivery</h2>
             </div>
           </div>
         </div>
-        <div className="w-full rounded-lg p-2 bg-white flex flex-col gap-3 text-center shadow-xl/10">
-          <h2 className="text-2xl font-medium pb-2">Order Summary</h2>
-          <div className="flex justify-between items-center text-lg font-medium">
-            <h2>Total:</h2>
-            <span> {total} </span>
+        {/* Order Summary */}
+        <div className="bg-gray-200 m-2 p-2 rounded-lg flex flex-col gap-2">
+          <h2 className="my-3 text-lg font-medium">Order Summary</h2>
+          <div className="bg-white rounded-lg text-lg font-medium p-2 flex justify-between">
+            <p>Grand Total: </p>
+            <p>{subTotal}</p>
           </div>
-          <button className="bg-primary p-2 rounded-2xl text-white font-bold uppercase cursor-pointer">
+          <button className="bg-primary/70 hover:bg-primary rounded-lg text-lg font-medium p-2">
             Place Order
           </button>
         </div>
