b5271229809219386a4ccc09f54d9ba2f2ef18a4
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-12-13 23:09:05 +0600

    Update: Added Transition to mobile Nav

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router";
 import { products } from "../data/products";
+import { CartContext } from "../context/CartContext";
 
 const ProductDetails = () => {
+  const { addToCart } = useContext(CartContext);
   const { code } = useParams();
   const product = products.find((p) => p.code === Number(code));
 
   if (!product) {
     return (
-      <div className="mx-auto container py-20 text-center">
+      <div className="mx-auto container py-20 text-center text-7xl">
         No products found...
       </div>
     );
@@ -18,9 +20,6 @@ const ProductDetails = () => {
 
   return (
     <div className="p-10">
-      <h2 className="text-2xl capitalize font-medium bg-slate-500 text-center h-fit mb-5">
-        Details of {name ?? "N/A"}
-      </h2>
       <div className="flex justify-center">
         <div className="flex flex-col justify-start gap-10 items-start md:flex-row ">
           <div className="h-100 w-fit border border-slate-500 rounded-lg shadow overflow-hidden">
@@ -30,6 +29,15 @@ const ProductDetails = () => {
             <h3>Name: {name ?? "N/A"}</h3>
             <p>Category: {category ?? "-"}</p>
             <p>Price: {price ?? 0}/-</p>
+            <button
+              className="bg-red-900 rounded-lg p-1 text-white cursor-pointer"
+              onClick={() => {
+                addToCart(product);
+                showToastNotification();
+              }}
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
