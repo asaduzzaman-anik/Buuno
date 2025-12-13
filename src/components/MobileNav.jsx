90b98adbefa6a32c109eb9081906cbb8a99a583a
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-12-13 23:09:05 +0600

    Update: Added Transition to mobile Nav

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -16,73 +16,87 @@ const MobileNav = () => {
   const { cart, openCart } = useContext(CartContext);
 
   return (
-    <nav className="w-full sm:px-[10%] sm:mx-auto left-0 right-0 flex justify-between items-center bg-white shadow sm:shadow-none h-14 sm:h-20 p-2">
-      {/* <!-- menu bar button + search icon for mobile screen --> */}
-      <div className="flex justify-between items-center gap-3 sm:hidden">
-        <button
-          type="button"
-          onClick={() => setOpen(!open)}
-          className="cursor-pointer"
-        >
-          <RiMenuFold2Line size={25} />
-        </button>
-        <button type="button" className="cursor-pointer">
-          <RiSearch2Line size={25} />
-        </button>
-      </div>
-      {/* <!-- brand logo --> */}
-      <Link to="/" className="cursor-pointer">
-        <img
-          src="/img/logo.png"
-          alt=""
-          className="h-full sm:h-18 sm:w-auto w-20"
-        />
-      </Link>
-      {/* <!-- search bar for large screen --> */}
-      <div className="hidden sm:flex">
-        <form action="" className="relative">
-          <input
-            type="text"
-            name="search-bar"
-            placeholder="Search products, brands or category"
-            className="w-[40vw] h-12 border rounded-lg border-gray-200 focus:border-red-700  px-2 text-xl"
-          />
+    <div>
+      <nav className="w-full sm:px-[10%] sm:mx-auto left-0 right-0 flex justify-between items-center bg-white shadow sm:shadow-none h-14 sm:h-20 p-2">
+        {/* <!-- menu bar button + search icon for mobile screen --> */}
+        <div className="flex justify-between items-center gap-3 sm:hidden">
           <button
-            type="submit"
-            className="bg-red-700 absolute top-0 right-0 rounded-tr-lg rounded-br-lg h-12 w-12 cursor-pointer"
+            type="button"
+            onClick={() => setOpen(!open)}
+            className="cursor-pointer"
           >
-            <img
-              src="/img/icons/search-white.png"
-              alt=""
-              className="h-10 w-10 p-2"
-            />
+            <RiMenuFold2Line size={25} />
           </button>
-        </form>
-      </div>
-      {/* <!-- cart icon + sign in option --> */}
-      <div className="flex justify-between items-center gap-3">
-        <div className="relative h-auto flex items-center">
-          <button onClick={openCart} className="cursor-pointer">
-            <RiShoppingCart2Line size={25} />
+          <button type="button" className="cursor-pointer">
+            <RiSearch2Line size={25} />
           </button>
-          {cart.length > 0 && (
-            <p className="absolute -top-3 -right-2 text-xs bg-red-900 text-white rounded-full w-5 h-5 text-center">
-              {cart.length}
-            </p>
-          )}
         </div>
-        <button type="button" className="cursor-pointer">
-          <RiUserLine size={25} />
-        </button>
-      </div>
+        {/* <!-- brand logo --> */}
+        <Link to="/" className="cursor-pointer">
+          <img
+            src="/img/logo.png"
+            alt=""
+            className="h-full sm:h-18 sm:w-auto w-20"
+          />
+        </Link>
+        {/* <!-- search bar for large screen --> */}
+        <div className="hidden sm:flex">
+          <form action="" className="relative">
+            <input
+              type="text"
+              name="search-bar"
+              placeholder="Search products, brands or category"
+              className="w-[40vw] h-12 border rounded-lg border-gray-200 focus:border-red-700  px-2 text-xl"
+            />
+            <button
+              type="submit"
+              className="bg-red-700 absolute top-0 right-0 rounded-tr-lg rounded-br-lg h-12 w-12 cursor-pointer"
+            >
+              <img
+                src="/img/icons/search-white.png"
+                alt=""
+                className="h-10 w-10 p-2"
+              />
+            </button>
+          </form>
+        </div>
+        {/* <!-- cart icon + sign in option --> */}
+        <div className="flex justify-between items-center gap-3">
+          <div className="relative h-auto flex items-center">
+            <button onClick={openCart} className="cursor-pointer">
+              <RiShoppingCart2Line size={25} />
+            </button>
+            {cart.length > 0 && (
+              <p className="absolute -top-3 -right-2 text-xs bg-red-900 text-white rounded-full w-5 h-5 text-center">
+                {cart.length}
+              </p>
+            )}
+          </div>
+          <button type="button" className="cursor-pointer">
+            <RiUserLine size={25} />
+          </button>
+        </div>
+      </nav>
+
       {/* MOBILE MENU */}
-      {open && (
-        <div className="absolute bg-red-500 top-14 left-0">
+      <div>
+        {open && (
+          <div
+            className="fixed inset-0 bg-black/50 z-50"
+            onClick={() => setOpen(false)}
+          ></div>
+        )}
+        <div
+          className={`fixed top-0 left-0 h-full w-64 bg-primary shadow-lg z-50 transform transition-transform duration-300 ${
+            open ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
           <ul className="flex flex-col gap-7 py-5 px-2 h-screen w-fit text-white">
             {categories.map((cat) => (
               <Link
                 to={cat.route_parameter}
                 key={cat.id}
+                onClick={() => setOpen(false)}
                 className="flex justify-start items-center"
               >
                 <img src={cat.icon} alt="" className="w-6 h-6 mr-2" />
@@ -91,8 +105,8 @@ const MobileNav = () => {
             ))}
           </ul>
         </div>
-      )}
-    </nav>
+      </div>
+    </div>
   );
 };
 
